2ac467a8c0cdb5ecd77fedb58d1e75b215774191
Aldo D'Aquino <daquino.aldo@gmail.com>
2018-02-20 20:51:22 +0100

    Webhook

---
+++ webhook.php
@@ -0,0 +1,35 @@
+<?php
+  /* CONFIGURATIONS */
+
+  // URL of the project you want to update with webhook
+  $project_url = "https://github.com/daquinoaldo/GitDeployOnHosting";
+
+  // PATH in which to deploy (ABSOLUTE!)
+  $folder = "/myprojects/GitDeployOnHosting";
+
+  // PATH of the GitDeployOnHosting index.php file
+  // Must NOT include index.php, start with http (or https) and end with "/"
+  $gdoh_path = "https://mydomain.com/deploy/";
+
+  // Username and password of GitDeployOnHosting
+  $username = "iamtheadmin";
+  $password = "yesitsme";
+
+
+
+  /* STOP! DO NOT EDIT */
+  $project_url = urlencode($project_url);
+  $folder = urlencode($folder);
+  $url = $gdoh_path."index.php?url=".$project_url."&folder=".$folder;
+
+  $curl_handle = curl_init();
+  curl_setopt($curl_handle, CURLOPT_URL, $url);
+  curl_setopt($curl_handle, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt($curl_handle, CURLOPT_CONNECTTIMEOUT, 10);
+  curl_setopt($curl_handle, CURLOPT_USERPWD, $username.":".$password);  
+  $html = curl_exec($curl_handle);
+  curl_close($curl_handle);
+
+  echo $html;
+
+?>
